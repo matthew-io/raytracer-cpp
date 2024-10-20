19d2703825f3266cc1c4a62c073303bdc9bb9ae5
matthew-io <xyzmonte@protonmail.com>
2024-10-20 10:36:43 +0100

    cant remember what ive done

--- build/CMakeFiles/raytracer.dir/main.cpp.o.d
+++ build/CMakeFiles/raytracer.dir/main.cpp.o.d
@@ -1,7 +1,6 @@
 CMakeFiles/raytracer.dir/main.cpp.o: \
  /home/matthew/Documents/raytracer/main.cpp /usr/include/stdc-predef.h \
- /home/matthew/Documents/raytracer/color.h \
- /home/matthew/Documents/raytracer/vec3.h /usr/include/c++/13/cmath \
+ /home/matthew/Documents/raytracer/rtweekend.h /usr/include/c++/13/cmath \
  /usr/include/c++/13/bits/requires_hosted.h \
  /usr/include/x86_64-linux-gnu/c++/13/bits/c++config.h \
  /usr/include/x86_64-linux-gnu/c++/13/bits/os_defines.h \
@@ -157,5 +156,31 @@ CMakeFiles/raytracer.dir/main.cpp.o: \
  /usr/include/c++/13/bits/locale_facets.tcc \
  /usr/include/c++/13/bits/basic_ios.tcc \
  /usr/include/c++/13/bits/ostream.tcc /usr/include/c++/13/istream \
- /usr/include/c++/13/bits/istream.tcc \
- /home/matthew/Documents/raytracer/ray.h
+ /usr/include/c++/13/bits/istream.tcc /usr/include/c++/13/limits \
+ /usr/include/c++/13/memory /usr/include/c++/13/bits/stl_tempbuf.h \
+ /usr/include/c++/13/bits/stl_uninitialized.h \
+ /usr/include/c++/13/bits/stl_raw_storage_iter.h \
+ /usr/include/c++/13/bits/align.h /usr/include/c++/13/bit \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /usr/include/c++/13/bits/uses_allocator.h \
+ /usr/include/c++/13/bits/unique_ptr.h /usr/include/c++/13/tuple \
+ /usr/include/c++/13/bits/shared_ptr.h \
+ /usr/include/c++/13/bits/shared_ptr_base.h \
+ /usr/include/c++/13/bits/allocated_ptr.h \
+ /usr/include/c++/13/ext/aligned_buffer.h \
+ /usr/include/c++/13/ext/concurrence.h \
+ /usr/include/c++/13/bits/shared_ptr_atomic.h \
+ /usr/include/c++/13/bits/atomic_base.h \
+ /usr/include/c++/13/bits/atomic_lockfree_defines.h \
+ /usr/include/c++/13/backward/auto_ptr.h \
+ /home/matthew/Documents/raytracer/color.h \
+ /home/matthew/Documents/raytracer/vec3.h \
+ /home/matthew/Documents/raytracer/ray.h \
+ /home/matthew/Documents/raytracer/hittable.h \
+ /home/matthew/Documents/raytracer/hittable_list.h \
+ /usr/include/c++/13/vector /usr/include/c++/13/bits/stl_vector.h \
+ /usr/include/c++/13/bits/stl_bvector.h \
+ /usr/include/c++/13/bits/vector.tcc \
+ /home/matthew/Documents/raytracer/sphere.h
